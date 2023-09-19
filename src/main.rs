b772ddd4c76d2c5201b4473313167bd2b2360615
Dawid Lentka <46752179+PingPongun@users.noreply.github.com>
2023-09-19 14:35:40 +0200

    update deps; bump version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Error;
-use clap::{App, Arg, SubCommand};
+use clap::{Arg, Command};
 
 use indexmap::IndexMap;
 use std::path::Path;
@@ -12,22 +12,23 @@ const ABOUT: &str = r#"Rust I18n command for help you simply to extract all untr
 It will iter all Rust files in and extract all untranslated texts that used `t!` macro.
 And then generate a YAML file and merge for existing texts.
 
-https://github.com/longbridgeapp/rust-i18n
+https://github.com/longbridgeCommand/rust-i18n
 "#;
 
 fn main() -> Result<(), Error> {
-    let extract_command = SubCommand::with_name("i18n")
+    let extract_command = Command::new("i18n")
         .about("Extract all untranslated I18n texts from soruce code")
         .version(clap::crate_version!())
         .arg(
-            Arg::with_name("source")
+            Arg::new("source")
                 .help("Path of your Rust crate root and Cargo.toml")
                 .default_value("./"),
         );
 
-    let app = App::new(APP_NAME)
+    let app = Command::new(APP_NAME)
         .bin_name("cargo")
         .about(ABOUT)
+        .subcommand_required(true)
         .subcommand(extract_command)
         .get_matches();
 
@@ -35,8 +36,10 @@ fn main() -> Result<(), Error> {
 
     #[allow(clippy::single_match)]
     match app.subcommand() {
-        ("i18n", Some(sub_m)) => {
-            let source_path = sub_m.value_of("source").expect("Missing source path");
+        Some(("i18n", sub_m)) => {
+            let source_path = sub_m
+                .get_one::<String>("source")
+                .expect("Missing source path");
 
             let cfg = rust_i18n_support::config::load(std::path::Path::new(source_path))?;
             iter::iter_crate(source_path, |path, source| {
